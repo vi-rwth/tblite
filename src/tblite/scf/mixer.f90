ff5920b48467f8f58a6e19a0f5c7a43f23ee3811
vi-rwth <133004724+vi-rwth@users.noreply.github.com>
2023-12-14 15:40:11 +0100

    Update mixer.f90

--- src/tblite/scf/mixer.f90
+++ src/tblite/scf/mixer.f90
@@ -41,16 +41,15 @@ module tblite_scf_mixer
 
 contains
 
-subroutine new_diis_mixer(self, ndim, overlap_matrix, memory)
+subroutine new_diis_mixer(self, overlap_matrix, memory)
    class(mixer_type), allocatable, intent(out) :: self
    integer, intent(in) :: memory
-   integer, intent(in) :: ndim
-   real(wp), intent(in) :: overlap_matrix(ndim,ndim)
+   real(wp), intent(in) :: overlap_matrix(:,:)
 
    block
       type(diis_mixer), allocatable :: mixer
       allocate(mixer)
-      call new_diis(mixer, ndim, overlap_matrix, diis_input(memory))
+      call new_diis(mixer, overlap_matrix, diis_input(memory))
       call move_alloc(mixer, self)
    end block
 end subroutine new_diis_mixer
