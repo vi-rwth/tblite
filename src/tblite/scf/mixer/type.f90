7a9f41bea9ad916faac8f640958dbc72a813f382
vi-rwth <133004724+vi-rwth@users.noreply.github.com>
2023-12-18 23:39:34 +0100

    Update type.f90

--- src/tblite/scf/mixer/type.f90
+++ src/tblite/scf/mixer/type.f90
@@ -28,13 +28,13 @@ module tblite_scf_mixer_type
    contains
       !> Apply mixing
       procedure(next), deferred :: next
-      !> Set new density
+      !> Set new density(q)
       generic :: set => set_1d, set_2d, set_3d
-      !> Set new density from 1D array
+      !> Set new density(q) from 1D array
       procedure(set_1d), deferred :: set_1d
-      !> Set new density from 2D array
+      !> Set new density(q) from 2D array
       procedure :: set_2d
-      !> Set new density from 3D array
+      !> Set new density(q) from 3D array
       procedure :: set_3d
       !> Set new Fock-matrix 
       generic :: set_F => set_1d_F, set_2d_F, set_3d_F
@@ -44,6 +44,14 @@ module tblite_scf_mixer_type
       procedure :: set_2d_F
       !> Set new Fock-matrix from 3D array
       procedure :: set_3d_F
+      !> Set new density
+      generic :: set_D => set_1d_D, set_2d_D, set_3d_D
+      !> Set new density from 1D array
+      procedure(set_1d_D), deferred :: set_1d_D
+      !> Set new density from 2D array
+      procedure :: set_2d_D
+      !> Set new density from 3D array
+      procedure :: set_3d_D
       !> Set difference between new and old density
       generic :: diff => diff_1d, diff_2d, diff_3d
       !> Set difference between new and old density from 1D array
@@ -52,13 +60,13 @@ module tblite_scf_mixer_type
       procedure :: diff_2d
       !> Set difference between new and old density from 3D array
       procedure :: diff_3d
-      !> Get density
+      !> Get density(q)
       generic :: get => get_1d, get_2d, get_3d
-      !> Get density as 1D array
+      !> Get density(q) as 1D array
       procedure(get_1d), deferred :: get_1d
-      !> Get density as 2D array
+      !> Get density(q) as 2D array
       procedure :: get_2d
-      !> Get density as 3D array
+      !> Get density(q) as 3D array
       procedure :: get_3d
       !> Get Fock-matrix
       generic :: get_F => get_1d_F, get_2d_F, get_3d_F
@@ -73,7 +81,7 @@ module tblite_scf_mixer_type
    end type mixer_type
 
    abstract interface
-      !> Set new density from 1D array
+      !> Set new density(q) from 1D array
       subroutine set_1d(self, qvec)
          import :: mixer_type, wp
          !> Instance of the electronic mixer
@@ -91,6 +99,15 @@ module tblite_scf_mixer_type
          real(wp), intent(in) :: f_1d(:)
       end subroutine set_1d_F
 
+      !> Set new density from 1D array
+      subroutine set_1d_D(self, d_1d)
+         import :: mixer_type, wp
+         !> Instance of the electronic mixer
+         class(mixer_type), intent(inout) :: self
+         !> Denisty-matrix
+         real(wp), intent(in) :: d_1d(:)
+      end subroutine set_1d_D
+
       !> Set difference between new and old density from 1D array
       subroutine diff_1d(self, qvec)
          import :: mixer_type, wp
@@ -100,7 +117,7 @@ module tblite_scf_mixer_type
          real(wp), intent(in) :: qvec(:)
       end subroutine diff_1d
 
-      !> Get density as 1D array
+      !> Get density(q) as 1D array
       subroutine get_1d(self, qvec)
          import :: mixer_type, wp
          !> Instance of the electronic mixer
@@ -161,6 +178,17 @@ subroutine set_3d(self, qvec)
    call self%set(qptr)
 end subroutine set_3d
 
+!> Set new Fock matrix from 2D array
+subroutine set_2d_F(self, f_2d)
+   !> Instance of the electronic mixer
+   class(mixer_type), intent(inout) :: self
+   !> Fock-matrix
+   real(wp), contiguous, intent(in), target :: f_2d(:, :)
+   real(wp), pointer :: f_2d_ptr(:)
+   f_2d_ptr(1:size(f_2d)) => f_2d
+   call self%set_F(f_2d_ptr)
+end subroutine set_2d_F
+
 !> Set new Fock matrix from 3D array
 subroutine set_3d_F(self, f_3d)
    !> Instance of the electronic mixer
@@ -172,16 +200,27 @@ subroutine set_3d_F(self, f_3d)
    call self%set_F(f_3d_ptr)
 end subroutine set_3d_F
 
-!> Set new Fock matrix from 2D array
-subroutine set_2d_F(self, f_2d)
+!> Set new density matrix from 2D array
+subroutine set_2d_D(self, d_2d)
    !> Instance of the electronic mixer
    class(mixer_type), intent(inout) :: self
-   !> Fock-matrix
-   real(wp), contiguous, intent(in), target :: f_2d(:, :)
-   real(wp), pointer :: f_2d_ptr(:)
-   f_2d_ptr(1:size(f_2d)) => f_2d
-   call self%set_F(f_2d_ptr)
-end subroutine set_2d_F
+   !> Density matrix
+   real(wp), contiguous, intent(in), target :: d_2d(:, :)
+   real(wp), pointer :: dptr(:)
+   dptr(1:size(d_2d)) => d_2d
+   call self%set_D(dptr)
+end subroutine set_2d_D
+
+!> Set new density matrix from 3D array
+subroutine set_3d_D(self, d_3d)
+   !> Instance of the electronic mixer
+   class(mixer_type), intent(inout) :: self
+   !> Density matrix
+   real(wp), contiguous, intent(in), target :: d_3d(:, :, :)
+   real(wp), pointer :: dptr(:)
+   dptr(1:size(d_3d)) => d_3d
+   call self%set_D(dptr)
+end subroutine set_3d_D
 
 !> Set difference between new and old density from 2D array
 subroutine diff_2d(self, qvec)
